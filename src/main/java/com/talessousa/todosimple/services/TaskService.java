3206a1fa2c0ab7759e9818652fa98798b7a87181
Tales <httaleps@gmail.com>
2025-10-04 23:51:59 -0300

    Create Task Controller

--- src/main/java/com/talessousa/todosimple/services/TaskService.java
+++ src/main/java/com/talessousa/todosimple/services/TaskService.java
@@ -1,5 +1,6 @@
 package com.talessousa.todosimple.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,8 +24,12 @@ public class TaskService {
     public Task findById(Long id) {
         Optional<Task> task = this.taskRepository.findById(id);
         return task.orElseThrow(() -> new RuntimeException(
-            "Tarefa não encontrada! Id: " + id + ", Tipo: " + Task.class.getName()
-        ));
+            "Tarefa não encontrada! Id: " + id + ", Tipo: " + Task.class.getName()));
+    }
+
+    public List<Task> findAllByUserId (Long userId) {
+        List<Task> tasks = this.taskRepository.findByUser_Id(userId);
+        return tasks;
     }
 
     @Transactional
